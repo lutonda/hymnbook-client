dcc3171e539e8ad9d937ef74de21aece0e3db9b7
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-08-31 18:26:52 +0100

    hymn implementation fix

--- src/app/pages/hymns/hymns.component.ts
+++ src/app/pages/hymns/hymns.component.ts
@@ -1,15 +1,28 @@
+import { HymnService } from 'app/services/hymn.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-hymns',
   templateUrl: './hymns.component.html',
   styleUrls: ['./hymns.component.css']
 })
-export class HymnsComponent implements OnInit {
 
-  constructor() { }
+export class HymnsComponent implements OnInit {
+  hymn={title:'...',parts:[]};
+  order=0;
+  incress(){
+    this.order++;
+  }
+  constructor(private service: HymnService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    let id=this.route.snapshot.paramMap.get('id');
+    this.service.getOneHymn(id).subscribe(data=>{
+      this.hymn=data.data
+    })
   }
+
 
 }
