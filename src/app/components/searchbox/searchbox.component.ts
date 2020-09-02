197869472da11cca84752efbb094ef609aa857af
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-09-02 15:27:49 +0100

    search fix

---
+++ src/app/components/searchbox/searchbox.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { HymnService } from 'app/services/hymn.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-searchbox',
+  templateUrl: './searchbox.component.html',
+  styleUrls: ['./searchbox.component.css']
+})
+export class SearchboxComponent implements OnInit {
+
+  datas:Array<any>=[];
+
+  constructor(private hymnService: HymnService, private router: Router) { }
+
+  @Output() searchData=new EventEmitter();
+  filterValue:any={}
+  ngOnInit(): void {
+    this.hymnService.getAllHymns().subscribe((d)=>{
+        this.datas=d.data;
+    });
+  }
+  typeahead:string;
+  suggestions: Array<any>=[];
+
+  filter(item){
+    this.typeahead='';
+    this.suggestions=[];
+    this.searchData.emit(item)
+  }
+  selectEvent(item:any){
+    this.searchData=item;
+  }
+  suggest(){
+    this.suggestions = this.datas.filter(c => {
+      let final = c.number==this.typeahead || this.typeahead.length>2 && c.title.includes(this.typeahead[0].toUpperCase()+this.typeahead.substring(1).toLowerCase())
+      return final;
+    }).slice(0, 10);
+
+  }
+
+}
