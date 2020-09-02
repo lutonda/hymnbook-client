4a5a74528568233c440664e4266a832e08cda450
PedroSousa2020 <sousapedro747@gmail.com>
2020-09-02 14:18:59 +0100

    Routerlink about hymn page on trafic

--- src/app/components/searchbox/searchbox.component.ts
+++ src/app/components/searchbox/searchbox.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HymnService } from 'app/services/hymn.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-searchbox',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchboxComponent implements OnInit {
 
-  constructor() { }
+  datas:Array<any>=[];
+
+  constructor(private hymnService: HymnService, private router: Router) { }
 
   ngOnInit(): void {
+    this.hymnService.getAllHymns().subscribe((d)=>{
+        this.datas=d.data;
+    });
   }
 
+  typeahead:string;
+  suggestions: Array<any>=[];
+
+  suggest(){
+    this.suggestions = this.datas.filter(c => {
+      let final = c.number==this.typeahead || this.typeahead.length>2 && c.title.includes(this.typeahead[0].toUpperCase()+this.typeahead.substring(1).toLowerCase())
+      return final;
+    }).slice(0, 10);
+
+  }
+  
 }
