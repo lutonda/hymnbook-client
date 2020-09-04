7ca95097b9e3823f72d033d6909a8e3dc190d34c
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-09-04 14:25:56 +0100

    hymn create componente work

--- src/app/admin/hymns/create-hymn/create-hymn.component.ts
+++ src/app/admin/hymns/create-hymn/create-hymn.component.ts
@@ -1,10 +1,14 @@
+import { TypePart } from './../../../models/type-part';
+import { Language } from './../../../models/language';
+import { Hymn } from './../../../models/hymn';
 
 import { TypePartService } from './../../../services/type-part.service';
 import { LanguageService } from './../../../services/language.service';
-import { ActivatedRoute } from '@angular/router';
+
 import { NgForm, FormArray, FormBuilder, Validators, AbstractControl } from "@angular/forms";
 import { HymnService } from './../../../services/hymn.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Part } from 'app/models/part';
 
 @Component({
   selector: 'app-create-hymn',
@@ -13,17 +17,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class CreateHymnComponent implements OnInit {
 
-  hymn: any = {parts:[{typePart:{}}]};
-  parts_text: Array<string> = [];
-  parts_type: Array<string> = [];
-  languages: Array<any> = [];
-  typeParts: Array<any> = [];
+  hymn: Hymn = new Hymn();
+  languages: Array<Language> = [];
+  typeParts: Array<TypePart> = [];
 
   constructor(
     private hymnService: HymnService,
     private languagesService: LanguageService,
     private typePartService: TypePartService,
-    private route: ActivatedRoute,
     private fb: FormBuilder) { }
 
   @Output('newSubForm') submitFormObjectToParent: EventEmitter<any> = new EventEmitter();
@@ -37,30 +38,22 @@ export class CreateHymnComponent implements OnInit {
   });
 
   ngOnInit(): void {
-   /* let id = this.route.snapshot.paramMap.get('id');
-    this.hymnService.getOneHymn(id).subscribe(data => this.hymn = data.data)*/
-    this.languagesService.getAlllanguages().subscribe(data => {
-      this.languages = data.data
-    })
+    this.languagesService.getAlllanguages().subscribe(data => this.languages = data.data)
     this.typePartService.getAllTypePart().subscribe(data => this.typeParts = data.data)
   }
   save() {
     alert(JSON.stringify(this.hymn))
-    /*this.hymnService.create(this.hymn).subscribe(data=>{
+    this.hymnService.create(this.hymn).subscribe(data=>{
       let u=data;
-    });*/
+    });
   }
 
   addPart() {
-    this.hymn.parts.push({ text: '', typePart: {} })
+    this.hymn.parts.push(new Part())
   }
 
   compareFn(c1: any, c2: any): boolean {
     return c1 && c2 ? c1._id === c2._id : c1 === c2;
-  }
-  //-------------------
-
-  submit() {
   }
 
   minFormArrayLength(min: number) {
