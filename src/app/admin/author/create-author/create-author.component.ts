fab5f784e18854093501a8c32004db2fa2587b10
PedroSousa2020 <sousapedro747@gmail.com>
2020-09-05 13:54:52 +0100

    list, create, upadate done but doesnt work

--- src/app/admin/author/create-author/create-author.component.ts
+++ src/app/admin/author/create-author/create-author.component.ts
@@ -1,15 +1,38 @@
+import { Author } from './../../../models/author';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AuthorService } from './../../../services/author.service';
 import { Component, OnInit } from '@angular/core';
+
+
+let id='';
 
 @Component({
   selector: 'app-create-author',
   templateUrl: './create-author.component.html',
   styleUrls: ['./create-author.component.css']
 })
+
 export class CreateAuthorComponent implements OnInit {
 
-  constructor() { }
+  author:Author=new Author(); 
+
+  constructor(private service: AuthorService, 
+              private route:ActivatedRoute, 
+              private fb:FormBuilder
+            ) { }
+
+  form= this.fb.group({
+        name: ['', Validators.required],
+        description: ['', Validators.required]
+
+  });
 
   ngOnInit(): void {
+  }
+
+  save(){
+    this.service.create(this.author).subscribe(data => console.log(data));
   }
 
 }
--- src/app/admin/author/update-author/update-author.component.ts
+++ src/app/admin/author/update-author/update-author.component.ts
@@ -1,15 +1,42 @@
+import { Author } from './../../../models/author';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AuthorService } from './../../../services/author.service';
 import { Component, OnInit } from '@angular/core';
+
+
+let id='';
 
 @Component({
   selector: 'app-update-author',
   templateUrl: './update-author.component.html',
   styleUrls: ['./update-author.component.css']
 })
+
 export class UpdateAuthorComponent implements OnInit {
 
-  constructor() { }
+  author:Author=new Author(); 
+
+  constructor(private service: AuthorService, 
+              private route:ActivatedRoute, 
+              private fb:FormBuilder
+            ) { }
+
+  form= this.fb.group({
+        name: ['', Validators.required],
+        description: ['', Validators.required]
+
+  });
 
   ngOnInit(): void {
+      id = this.route.snapshot.paramMap.get('id');
+      
+      this.service.getOneAuthor(id).subscribe(data=>{this.author=data.data;});
+  }
+
+  save(){
+    console.log(this.author._id);
+    this.service.updateAuthor(this.author).subscribe(data => console.log(data));
   }
 
 }
