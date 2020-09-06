d110f18c68511eee85f4e90441a96c018edf8bf8
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-09-06 23:35:29 +0100

    file uploder

--- src/app/models/hymn.ts
+++ src/app/models/hymn.ts
@@ -8,10 +8,10 @@ export class Hymn {
   _id: string;
   title: string;
   number: number;
-  author: Author = new Author();
+  author: Author;//new Author();
   parts: Array<Part> = [];
   language: Language = new Language();
-
+  files: Array<any> = [];
   constructor(title?: string, num?: number) {
     this.title = title;
     this.number = num;
--- src/app/pages/hymns/hymns.component.ts
+++ src/app/pages/hymns/hymns.component.ts
@@ -37,7 +37,7 @@ export class HymnsComponent implements OnInit {
   }
   // Material Style Basic Audio Player Title and Audio URL
   msbapTitle = 'Audio Title';
-  msbapAudioUrl = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  msbapAudioUrl = 'https://doc-14-a4-docs.googleusercontent.com/docs/securesc/2qlrtbj35evetsbk7qsstebeh85tfvt4/sq5qsb31qd3oubnls2up7sdb02stj743/1599430575000/06750515141549952095/06750515141549952095/18sdv---_b07XQ6MIgdgO204IhSSj9Uoc?e=download&authuser=0';
 
   msbapDisplayTitle = false;
   msbapDisplayVolumeControls = true;
