185bfab9515f80e0b4e9f6e7fab2429412a3bd68
PedroSousa2020 <sousapedro747@gmail.com>
2020-09-08 14:14:08 +0100

    sigIn e logOut funcionando e guardando os dados

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+let identity = 'users';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class UserService {
+
+
+  constructor(private http: HttpClient){
+  }
+
+  create(data: any):Observable<any>{
+
+    let u = JSON.parse('{"name": "'+data.name+'", "email": "' +data.email+'"}');
+
+    alert(u.email);
+
+    return this.http.post('http://127.0.0.1:8800/api/v1/users/new', u);
+  }
+}
